a71422a98de6fb582b1a151de6e1ad8c544ae8ab
Vil <vitalik.levchuk.008@gmail.com>
2025-01-05 22:42:06 +0300

    Add additional modifications to @Feedback

--- src/main/java/org/fbs/mcb/annotation/Feedback.java
+++ src/main/java/org/fbs/mcb/annotation/Feedback.java
@@ -1,13 +1,14 @@
 package org.fbs.mcb.annotation;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.*;
 
 /**
  * This annotation is used to mark methods that handle feedback messages in a Telegram bot.
  * It provides a way to specify the type of feedback that a method can handle.
  */
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Documented
 public @interface Feedback {
 
     /**
