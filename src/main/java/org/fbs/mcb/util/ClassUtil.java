5be6f9e1295970aa2a5d56578f39d78f56b8e945
Vil <vitalik.levchuk.008@gmail.com>
2025-01-07 16:31:48 +0300

    Rename parameters

--- src/main/java/org/fbs/mcb/util/ClassUtil.java
+++ src/main/java/org/fbs/mcb/util/ClassUtil.java
@@ -15,23 +15,23 @@ public class ClassUtil {
     /**
      * Checks if the specified class is either the same as, or is a superclass or superinterface of, the specified class.
      *
-     * @param ancestor The class to be checked for being an ancestor of the {@code descendant} class.
-     * @param descendant The class to be checked for being a descendant of the {@code ancestor} class.
-     * @return {@code true} if the {@code ancestor} class is either the same as, or is a superclass or superinterface of, the {@code descendant} class;
+     * @param parent The class to be checked for being a parent of the {@code child} class.
+     * @param child The class to be checked for being a child of the {@code parent} class.
+     * @return {@code true} if the {@code parent} class is either the same as, or is a superclass or superinterface of, the {@code child} class;
      *         {@code false} otherwise.
      */
-    public static boolean isAssignableFrom(Class<?> ancestor, Class<?> descendant) {
-        Class<?> currentClass = descendant;
-        if (ancestor == descendant){
+    public static boolean isAssignableFrom(Class<?> parent, Class<?> child) {
+        Class<?> currentClass = child;
+        if (parent == child){
             return true;
         }
         while (currentClass != null) {
-            if (currentClass.equals(ancestor)) {
+            if (currentClass.equals(parent)) {
                 return true;
             }
             Type[] interfaces = currentClass.getGenericInterfaces();
             for (Type iFace : interfaces) {
-                if (isAssignableFrom(ancestor, (Class<?>) iFace)) {
+                if (isAssignableFrom(parent, (Class<?>) iFace)) {
                     return true;
                 }
             }
