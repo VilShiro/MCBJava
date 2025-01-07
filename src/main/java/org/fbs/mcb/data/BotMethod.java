01bfc471f6d12797ec6831d7a8b5242f95104dc8
Vil <vitalik.levchuk.008@gmail.com>
2025-01-07 17:22:54 +0300

    MethodSignature type signature removed from constants

--- src/main/java/org/fbs/mcb/data/BotMethod.java
+++ src/main/java/org/fbs/mcb/data/BotMethod.java
@@ -13,7 +13,6 @@ import java.lang.reflect.Method;
 public class BotMethod {
 
     private final Method method;
-    private final MethodSignature signature;
     private final boolean inNewThread;
     private final ClassReorder reorder;
     
@@ -30,7 +29,6 @@ public class BotMethod {
      * Represents a method that can be invoked by a bot.
      *
      * @param method The method to be associated with this BotMethod instance.
-     * @param signature The signature of the method.
      * @param inNewThread Indicates whether the method should be invoked in a new thread.
      * @param ignoreTypes An array of IgnoreType values indicating which types of messages should be ignored when invoking the method.
      * @param ignoreSenders An array of IgnoreSender values indicating which senders should be ignored when invoking the method.
@@ -38,9 +36,8 @@ public class BotMethod {
      * @param key A unique identifier for the bot method.
      * @param reorder An instance of ClassReorder to handle class reordering.
      */
-    public BotMethod(Method method, MethodSignature signature, boolean inNewThread, IgnoreType[] ignoreTypes, IgnoreSender[] ignoreSenders, MethodType type, String key, ClassReorder reorder) {
+    public BotMethod(Method method, boolean inNewThread, IgnoreType[] ignoreTypes, IgnoreSender[] ignoreSenders, MethodType type, String key, ClassReorder reorder) {
         this.method = method;
-        this.signature = signature;
         this.inNewThread = inNewThread;
         this.ignoreTypes = ignoreTypes;
         this.ignoreSenders = ignoreSenders;
