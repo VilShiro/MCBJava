4eddafa15919dcb74ea22f4006f44910826cb1df
Vil <vitalik.levchuk.008@gmail.com>
2025-01-07 23:46:16 +0300

    Some other edits

--- src/test/java/org/fbs/mcb/AppTest.java
+++ src/test/java/org/fbs/mcb/AppTest.java
@@ -4,9 +4,16 @@ import org.junit.Test;
 
 public class AppTest{
 
+    private static class A{}
+    private static class B extends A{}
+
     @Test
     public void startUpTest(){
 
+    }
+
+    public static void main(String[] args){
+        new TestBot();
     }
 
 }
