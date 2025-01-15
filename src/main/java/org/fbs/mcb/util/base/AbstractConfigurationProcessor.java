16627ca5b5230ee58127301229abe1e492480751
Vil <vitalik.levchuk.008@gmail.com>
2025-01-15 17:09:44 +0300

    Adding documentation to AbstractConfigurationProcessor class

--- src/main/java/org/fbs/mcb/util/base/AbstractConfigurationProcessor.java
+++ src/main/java/org/fbs/mcb/util/base/AbstractConfigurationProcessor.java
@@ -1,7 +1,17 @@
 package org.fbs.mcb.util.base;
 
+/**
+ * This is an abstract class that provides a basic structure for processing configurations.
+ * It defines an abstract method called {@link #handle(Object...)} which should be implemented by subclasses.
+ */
 public abstract class AbstractConfigurationProcessor {
 
+    /**
+     * This method should be implemented by subclasses to handle configurations.
+     * It accepts a variable number of arguments of type {@link Object}.
+     *
+     * @param args The arguments to be processed.
+     */
     public abstract void handle(Object ... args);
 
 }
